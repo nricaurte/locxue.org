b89f58f912ce446c39575e558364db06806b17c4
ghgarcia1127 <ghgarcia1127@gmail.com>
2015-10-27 19:58:10 -0500

    boton de redes sociales y navegabilidad de pagina

--- web/js/init.js
+++ web/js/init.js
@@ -3,16 +3,22 @@ $(function () {
   // definir mediente $pos la altura en píxeles desde el borde superior de la ventana del navegador y el elemento
   var $pos = 140;
   var $sliderAltura = $('#elSlider').height();
+
+      
   $win.scroll(function () {
 
-    if ($win.scrollTop() <= $pos){
+    if ($win.scrollTop() <= $sliderAltura-$pos){
         $("nav").removeClass("blue lighten-3").addClass("transparent");
-		$("nav a").removeClass("black-text").addClass("white-test");
+		$(".texto-nav").removeClass("black-text").addClass("white-text");
 		$("#logo").removeClass("peque").addClass("grande");
+		$("#soclal-button").removeClass("bajo-slider").addClass("sobre-slider");
+		$("#nav-page-wrapper").removeClass("bajo-slider").addClass("sobre-slider");
     } else{
         $("nav").removeClass("transparent").addClass("blue lighten-3");
-		$("nav a").removeClass("while-text").addClass("black-text");
+		$(".texto-nav").removeClass("white-text").addClass("black-text");
 		$("#logo").removeClass("grande").addClass("peque");
+		$("#soclal-button").removeClass("sobre-slider").addClass("bajo-slider");
+		$("#nav-page-wrapper").removeClass("sobre-slider").addClass("bajo-slider");
     }
 });
 $(window).resize(function() {
@@ -21,6 +27,7 @@ $(window).resize(function() {
 });
 $(document).ready(function(){
       $('.slider').slider({full_width: true});
+	  $('.slider2').slider({full_width: true});
 	  $('.parallax').parallax();
 	  $('.scrollspy').scrollSpy();
 	  $('.contenido').css('top', $sliderAltura);
